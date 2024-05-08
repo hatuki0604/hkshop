661276099a7734a6fc35d36b94b5eb29a2388d1b
Hà Trung Kiên <128976476+hatuki0604@users.noreply.github.com>
2024-05-08 23:57:16 +0700

    tai len ne

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../Components/Hero/Hero"
+import Popular from "../Components/Popular/Popular";
+import Offers from "../Components/Offers/Offers";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
+
+const Shop = () => {
+    return(
+        <div>
+            <Hero/>
+            <Popular/>
+            <Offers/>
+            <NewsLetter/>
+        </div>
+    )
+}
+
+export default Shop; 
